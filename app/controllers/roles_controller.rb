3a33985c033285bdaea35b002aef94c75e60af54
Seba <seba.orellana.aguilera@gmail.com>
2021-10-27 14:57:05 -0300

    agregando la vista de crear y listar un rol

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,6 +1,20 @@
 class RolesController < ApplicationController
+    def listar
+        @roles = Rol.all
+    end
 
     def crear
-    @mi_nombre= "Seba El Cuatico" 
+    @mi_nombre = "Seba El Cuatico" 
+    @rol = Rol.new
     end          
+    def guardar
+        datos_rol = params.require(:rol).permit(:rol)
+        @nuevo_rol = Rol.new(datos_rol)
+        if @nuevo_rol.save
+            redirect_to roles_path
+        else
+            
+        end
+        
+    end
 end
